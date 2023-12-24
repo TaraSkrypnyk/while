fd767ff8fe5087aee6437639ab3b62a34cb3274d
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-24 17:25:04 +0200

    Ex3  was done

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 try:
 
+
+
+
+
+
+
+
+
+
+    p = int(input("Введіть сторону квадрата "))
+    x = y = p
+    i = 0
+    j = 0
+    while i < x:
+        j = 0
+        while j < y:
+            if ((i + j) >= (p - 1)) and (i >= j):
+                print("[" + str(i) + " " + str(j) + "]", end="")
+
+            else:
+                print("     ", end="")
+            j += 1
+        print()
+        i += 1
+
 except Exception as e:
     print (e)
