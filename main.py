6bc7b7aa5afc40483e8a4727a26ab20c3a2e534c
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-25 09:30:07 +0200

    Add new tamplate

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 try:
+    p = int(input("Введіть сторону квадрата "))
+    x = y = p
+    def top():
+        i = 0
+        j = 0
+        while i < x:
+            j = 0
+            while j < y:
+                if ((i + j) <= (p - 1)) and (i <= j):
+                    print("* ", end="")
+                else:
+                    print("  ", end="")
+                j += 1
+            print()
+            i += 1
+    def right():
+
+
+
 
 except Exception as e:
     print (e)
