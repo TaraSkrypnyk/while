7156185cbe8c21c145cfb2b3db8086ae885f45f8
ТараСкрипник <garoldskrypnyk@gmail.com>
2023-12-25 10:20:43 +0200

    Ex was done

--- main.py
+++ main.py
@@ -1,49 +1,26 @@
+from math import*
 try:
     p = int(input("Введіть сторону квадрата "))
     x = y = p
-    def top():
-        i = 0
+    i = 0
+    j = 0
+    while i < x:
         j = 0
-        while i < x:
-            j = 0
-            while j < y:
-                if ((i + j) <= (p - 1)) and (i <= j):
-                    print("* ", end="")
-                else:
-                    print("  ", end="")
-                j += 1
-            print()
-            i += 1
-    def bottom():
-        i = 0
-        j = 0
-        while i < x:
-            j = 0
-            while j < y:
-                if ((i + j) >= (p - 1)) and (i >= j):
-                    print("* ", end="")
-
-                else:
-                    print("  ", end="")
-                j += 1
-            print()
-            i += 1
-    def left():
-        i = 0
-        j = 0
-        while i < x:
-            j = 0
-            while j < y:
-                if ((i + j) <= (p - 1)) and i >= j:
-                    print("* ", end="")
-                else:
-                    print("  ", end="")
-                j += 1
-            print()
-            i += 1
-    def right():
-
-
-
+        while j < y:
+            if ((i + j) < (floor(p/2))):
+                print("     ", end="")
+            elif ((i + j) > (p+(floor(p/2)))):
+                print("     ", end="")
+            elif ((i + j) >= (p+(floor(p/2)))):
+                print("     ", end="")
+            elif ((i - j) > (floor(p/2))):
+                print("     ", end="")
+            elif ((j - i) > (floor(p/2))):
+                print("     ", end="")
+            else:
+                print("[" + str(i) + " " + str(j) + "]", end="")
+            j += 1
+        print()
+        i += 1
 except Exception as e:
     print (e)
